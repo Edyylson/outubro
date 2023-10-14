04bc267e17cd9e015cd4951b233dac0c4b6995a5
edilson <141554522+Edyylson@users.noreply.github.com>
2023-10-14 12:50:38 -0300

    atualizando

--- java.js
+++ java.js
@@ -5,4 +5,29 @@
 
 //alert(soma);
 
+let soma = 0;
+let somaSaida = 0;
+
+
+
+for (let i = 0; i < 8; i++) {
+
+
+  entrada = document.querySelectorAll(".vlentrada");
+  saida = document.querySelectorAll(".vlsaida")
+  soma = Number(entrada[i].textContent) + soma;
+  somaSaida = Number(saida[i].textContent) + somaSaida;
+
+
+}
+
+let cxEntrada = document.querySelector(".entrOutubro");
+cxEntrada.innerHTML = soma;
+let cxSaida = document.querySelector(".saidOutubro");
+cxSaida.innerHTML = somaSaida;
+
+let cxSaldo = document.querySelector(".vlSaldo");
+cxSaldo.innerHTML = Math.ceil(Number(cxEntrada.innerHTML) - Number(cxSaida.innerHTML) + 434);
+
+
 
