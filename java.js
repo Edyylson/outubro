808d7a20bbfad81f31d9862eb44901b6db31f1db
edilson <141554522+Edyylson@users.noreply.github.com>
2023-10-15 12:06:23 -0300

    salvar

--- java.js
+++ java.js
@@ -5,17 +5,25 @@ const cxSaida = document.querySelector(".saidOutubro");
 const cxSaldo = document.querySelector(".vlSaldo");
 const entrada = document.querySelectorAll(".vlentrada");
 const saida = document.querySelectorAll(".vlsaida");
+const BdGastos = document.getElementsByClassName("gastos");
 
 
-for (let i = 0; i < 8; i++) {
+//for (let i = 0; i < 8; i++) {
 
 
-  soma = Number(entrada[i].textContent) + soma;
-  somaSaida = Number(saida[i].textContent) + somaSaida;
+for (let i = 0; i < 15; i++) {
+
+  try {
+    if (entrada[i].innerHTML > 0) {
+      soma = Number(entrada[i].textContent) + soma;
+      somaSaida = Number(saida[i].textContent) + somaSaida;
+    }
+  } catch { break };
+
 }
-
 
 cxEntrada.innerHTML = soma;
 cxSaida.innerHTML = somaSaida;
 cxSaldo.innerHTML = Math.ceil(Number(cxEntrada.innerHTML) - Number(cxSaida.innerHTML) + 434);
+
 
