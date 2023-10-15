19ed9ffe40e6a68cd15ef1599d364975b0eae4ba
edilson <141554522+Edyylson@users.noreply.github.com>
2023-10-15 17:36:45 -0300

    ajustando

--- java.js
+++ java.js
@@ -1,3 +1,5 @@
+//DECLARANDO VARIAVEIS;
+
 let soma = 0;
 let somaSaida = 0;
 const cxEntrada = document.querySelector("#entrOutubro");
@@ -7,8 +9,10 @@ const entrada = document.querySelectorAll(".vlentrada");
 const saida = document.querySelectorAll(".vlsaida");
 const BdGastos = document.getElementsByClassName("gastos");
 
-for (let i = 0; i < 20; i++) {
+//LOOP SOMAR VALORES ENTRADAS, SAIDAS, E SALDO PARA AS VARIAVEIS AUTOMATICAMENTE
 
+for (let i = 0; i < 20; i++) {
+  //TRY E CATCH SÃO PARA TRATAMENTOS DE ERRO;
   try {
 
 
@@ -17,10 +21,11 @@ for (let i = 0; i < 20; i++) {
       somaSaida = Number(saida[i].textContent) + somaSaida;
     }
 
-  } catch { break };
+  } catch { break }; //BREAK É PARA FINALIZAR LOOP.
 
 }
 
+// ATRIBUINDO VALORES AS VARIAVEIS DE DOCUMENTOS DOM.
 cxEntrada.innerHTML = soma
 cxSaida.innerHTML = somaSaida
 cxSaldo.innerHTML = Math.ceil(Number(cxEntrada.innerHTML) - Number(cxSaida.innerHTML) + 434);
