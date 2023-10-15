b54c0670ce4c892de8b0142fc9b8d3913a568c3e
edilson <141554522+Edyylson@users.noreply.github.com>
2023-10-15 10:46:01 -0300

    atualizando

--- java.js
+++ java.js
@@ -1,33 +1,21 @@
-//let vl = document.querySelector("#valo");
-//let vlr = document.querySelector("#valor");
-//let soma = Number(vl.textContent) + Number(vlr.textContent);
-//let somas = 10 + 30;
-
-//alert(soma);
-
 let soma = 0;
 let somaSaida = 0;
-
+const cxEntrada = document.querySelector(".entrOutubro");
+const cxSaida = document.querySelector(".saidOutubro");
+const cxSaldo = document.querySelector(".vlSaldo");
+const entrada = document.querySelectorAll(".vlentrada");
+const saida = document.querySelectorAll(".vlsaida");
 
 
 for (let i = 0; i < 8; i++) {
 
 
-  entrada = document.querySelectorAll(".vlentrada");
-  saida = document.querySelectorAll(".vlsaida")
   soma = Number(entrada[i].textContent) + soma;
   somaSaida = Number(saida[i].textContent) + somaSaida;
-
-
 }
 
-let cxEntrada = document.querySelector(".entrOutubro");
+
 cxEntrada.innerHTML = soma;
-let cxSaida = document.querySelector(".saidOutubro");
 cxSaida.innerHTML = somaSaida;
-
-let cxSaldo = document.querySelector(".vlSaldo");
 cxSaldo.innerHTML = Math.ceil(Number(cxEntrada.innerHTML) - Number(cxSaida.innerHTML) + 434);
-
-
 
