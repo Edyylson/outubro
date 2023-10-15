69f12c36f57bbaf868c39dc4a698263a1893fddd
edilson <141554522+Edyylson@users.noreply.github.com>
2023-10-15 17:30:05 -0300

    salvar

--- java.js
+++ java.js
@@ -1,13 +1,11 @@
 let soma = 0;
 let somaSaida = 0;
-const cxEntrada = document.querySelector(".entrOutubro");
-const cxSaida = document.querySelector(".saidOutubro");
-const cxSaldo = document.querySelector(".vlSaldo");
+const cxEntrada = document.querySelector("#entrOutubro");
+const cxSaida = document.querySelector("#saidOutubro");
+const cxSaldo = document.querySelector("#vlSaldo");
 const entrada = document.querySelectorAll(".vlentrada");
 const saida = document.querySelectorAll(".vlsaida");
 const BdGastos = document.getElementsByClassName("gastos");
-
-
 
 for (let i = 0; i < 20; i++) {
 
@@ -23,8 +21,19 @@ for (let i = 0; i < 20; i++) {
 
 }
 
-cxEntrada.innerHTML = soma;
-cxSaida.innerHTML = somaSaida;
+cxEntrada.innerHTML = soma
+cxSaida.innerHTML = somaSaida
 cxSaldo.innerHTML = Math.ceil(Number(cxEntrada.innerHTML) - Number(cxSaida.innerHTML) + 434);
 
+//FORMATAÇÃO DOS VALORES EM MOEDAS BR;
+cxEntrada.innerHTML = soma.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+cxSaida.innerHTML = somaSaida.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+cxSaldo.innerHTML = "R$" + "" + " " + cxSaldo.innerHTML;
 
+
+/*function somarValores(i) {
+
+  alert(i);
+}
+
+let chamarFunction = somarValores(300)*/
