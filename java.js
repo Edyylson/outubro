dbdc7a2f57fc785a7b22d1f1b9edbbc2d39017ef
edilson <141554522+Edyylson@users.noreply.github.com>
2023-10-23 20:29:41 -0300

    salvar

--- java.js
+++ java.js
@@ -1,25 +1,3 @@
-
-
-
-/*
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "financas"
-});
-
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});*/
-
-
-
-
-
 //DECLARANDO VARIAVEIS;
 
 let soma = 0;
